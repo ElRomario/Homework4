fb479b69ae31de030baaba598e8a744075271049
ElRomario <hellishassailant007@gmail.com>
2023-03-14 15:13:57 +0300

    homework done

---
+++ task1/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int pow(int numb, int pow)
+{
+    int product = numb;
+    for (int i = 1; i < pow; i++)
+    {
+        product *=numb;
+    }
+    return product;
+}
+Console.WriteLine("Enter number:");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter pow:");
+int power = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"number {number} to the power of {power} = {pow(number, power)}");
---
+++ task2/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12*/
+
+
+int digitsCounter(int number)
+{
+    int[] array = new int[number.ToString().Length];
+    for(int i =0; i < array.Length; i++)
+    {
+        array[i] = number % 10;
+        number /= 10;
+    }
+    Array.Reverse(array);
+
+    int digitCount = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        digitCount += array[i]; 
+    }
+    return digitCount;
+
+}
+
+Console.WriteLine("Enter the number:");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"{number} -> {digitsCounter(number)}");
